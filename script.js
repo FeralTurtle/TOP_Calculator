1a7512edc2bda84abc1858ecea09fe2d4050161a
Jeremy T <jeremytupas@gmail.com>
2022-01-18 20:49:57 -0600

    Revising approach and simplifying functions. Added addBtnClickEvents() and gatherInput() functions.

--- script.js
+++ script.js
@@ -29,28 +29,42 @@ function operate(operator, a, b) {
 function makeButtons() {
     for (let i = 0; i < tileCount; i++) {
         const newBtn = document.createElement("div");
-        const currentChar = numsArray[i];
+        const currentChar = symbolsArray[i];
         newBtn.textContent = currentChar;
-        populateDisplayEvent(newBtn, currentChar);
         gridContainer.appendChild(newBtn);
     }
 }
 
-function populateDisplayEvent(newBtn, currentChar) {
-    newBtn.onclick = () => {
-        displayValue = displayValue.concat(currentChar);
-        display.append(currentChar);
-    };
+function addBtnClickEvents() {
+    const btns = document.querySelectorAll(".nums div");
+    btns.forEach(btn => btn.addEventListener('click', gatherInput));
+}
+
+function gatherInput() {
+    let currentChar = this.textContent;
+    console.log(currentChar);
+    // console.log(typeof(currentChar));
+    if (operatorsArray.indexOf(currentChar) !== -1) { // Checks operatorsArray if currentChar isn't not (is) present.
+        console.log("is an operator");
+    } else if (numsArray.indexOf(currentChar) !== -1) {
+        console.log("is a number");
+    }
 }
 
 const display = document.querySelector(".display");
 const gridContainer = document.querySelector(".nums");
 const tileCount = 16;
-const numsArray = ["7", "8", "9", "/",
-                    "4", "5", "6", "x",
-                    "1", "2", "3", "-",
-                    "0", ".", "C", "+"];
+const symbolsArray = ["7", "8", "9", "/",
+                      "4", "5", "6", "x",
+                      "1", "2", "3", "-",
+                      "0", ".", "C", "+"];
+const numsArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const operatorsArray = ["/", "x", "-", "+", "."];
+let previousInput = "";
+let chosenOperator = "";
+let currentInput = "";
 let displayValue = "";
 display.append(displayValue);
 
 makeButtons();
+addBtnClickEvents();
